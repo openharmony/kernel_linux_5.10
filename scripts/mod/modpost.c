30b08604215fdd591b6dc27285fd39a32bfd31ca
Sami Tolvanen <samitolvanen@google.com>
2020-12-11 10:46:27 -0800

    modpost: lto: strip .lto from module names
    mainline inclusion
    from mainline-5.12-rc1
    commit 7ac204b545f263c7595a8b5ec3797ae9f9954f82
    category: feature
    feature: Clang LTO
    issue: I5MXBS
    CVE: NA
    
    Signed-off-by: GUO Zihua <guozihua@huawei.com>
    --------------------------------
    
    With LTO, everything is compiled into LLVM bitcode, so we have to link
    each module into native code before modpost. Kbuild uses the .lto.o
    suffix for these files, which also ends up in module information. This
    change strips the unnecessary .lto suffix from the module name.
    
    Suggested-by: Bill Wendling <morbo@google.com>
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Link: https://lore.kernel.org/r/20201211184633.3213045-11-samitolvanen@google.com
    Conflicts:
    	scripts/mod/modpost.c
    Signed-off-by: GUO Zihua <guozihua@huawei.com>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -80,14 +80,6 @@ modpost_log(enum loglevel loglevel, const char *fmt, ...)
 		exit(1);
 }
 
-static inline bool strends(const char *str, const char *postfix)
-{
-	if (strlen(str) < strlen(postfix))
-		return false;
-
-	return strcmp(str + strlen(str) - strlen(postfix), postfix) == 0;
-}
-
 void *do_nofail(void *ptr, const char *expr)
 {
 	if (!ptr)
@@ -1984,6 +1976,10 @@ static char *remove_dot(char *s)
 		size_t m = strspn(s + n + 1, "0123456789");
 		if (m && (s[n + m] == '.' || s[n + m] == 0))
 			s[n] = 0;
+
+		/* strip trailing .lto */
+		if (strends(s, ".lto"))
+			s[strlen(s) - 4] = '\0';
 	}
 	return s;
 }
@@ -2007,6 +2003,9 @@ static void read_symbols(const char *modname)
 		/* strip trailing .o */
 		tmp = NOFAIL(strdup(modname));
 		tmp[strlen(tmp) - 2] = '\0';
+		/* strip trailing .lto */
+		if (strends(tmp, ".lto"))
+			tmp[strlen(tmp) - 4] = '\0';
 		mod = new_module(tmp);
 		free(tmp);
 	}
