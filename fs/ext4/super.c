3bfe7e8031b3f2d49cf4afb6db62b4579855c739
Jan Kara <jack@suse.cz>
2021-08-06 11:33:52 +0800

    ext4: protect superblock modifications with a buffer lock
    mainline inclusion
    from mainline-v5.11-rc4
    commit 05c2c00f3769abb9e323fcaca70d2de0b48af7ba
    category: bugfix
    issue: #I3ZXZF
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=05c2c00f3769abb9e323fcaca70d2de0b48af7ba
    
    -----------------------------------------------
    
    Protect all superblock modifications (including checksum computation)
    with a superblock buffer lock. That way we are sure computed checksum
    matches current superblock contents (a mismatch could cause checksum
    failures in nojournal mode or if an unjournalled superblock update races
    with a journalled one). Also we avoid modifying superblock contents
    while it is being written out (which can cause DIF/DIX failures if we
    are running in nojournal mode).
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://lore.kernel.org/r/20201216101844.22917-4-jack@suse.cz
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    
    conflicts:
    fs/ext4/file.c
    
    Signed-off-by: Ye Bin <yebin10@huawei.com>
    Reviewed-by: zhangyi (F) <yi.zhang@huawei.com>
    Signed-off-by: Chen Jun <chenjun102@huawei.com>
    Signed-off-by: Yu Changchun <yuchangchun1@huawei.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -5460,6 +5460,7 @@ static int ext4_commit_super(struct super_block *sb)
 	if (block_device_ejected(sb))
 		return -ENODEV;
 
+	lock_buffer(sbh);
 	/*
 	 * If the file system is mounted read-only, don't update the
 	 * superblock write time.  This avoids updating the superblock
@@ -5531,7 +5532,6 @@ static int ext4_commit_super(struct super_block *sb)
 
 	BUFFER_TRACE(sbh, "marking dirty");
 	ext4_superblock_csum_set(sb);
-	lock_buffer(sbh);
 	if (buffer_write_io_error(sbh) || !buffer_uptodate(sbh)) {
 		/*
 		 * Oh, dear.  A previous attempt to write the
