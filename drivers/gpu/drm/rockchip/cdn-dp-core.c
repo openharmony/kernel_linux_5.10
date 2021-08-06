629a130d8a1c983da604034f21280db44f851495
Chen Jun <chenjun102@huawei.com>
2021-08-06 17:59:32 +0800

    rockchip: Make cdn_dp_resume depend on CONFIG_PM_SLEEP
    ohos inclusion
    category: bugfix
    issue: #I3ZXZF
    CVE: NA
    
    -------------------------------------------------------------------------
    
    If build Image without CONFIG_PM_SLEEP, there would be a compile
    warning:
    warning: ‘cdn_dp_resume’ defined but not used [-Wunused-function]
    
    Because SET_SYSTEM_SLEEP_PM_OPS will do nothing without CONFIG_PM_SLEEP.
    
    Make cdn_dp_resume depend on CONFIG_PM_SLEEP
    
    Signed-off-by: Chen Jun <chenjun102@huawei.com>
    Reviewed-by: Xie XiuQi <xiexiuqi@huawei.com>
    Signed-off-by: Chen Jun <chenjun102@huawei.com>
    Signed-off-by: Yu Changchun <yuchangchun1@huawei.com>

--- drivers/gpu/drm/rockchip/cdn-dp-core.c
+++ drivers/gpu/drm/rockchip/cdn-dp-core.c
@@ -1122,6 +1122,7 @@ static int cdn_dp_suspend(struct device *dev)
 	return ret;
 }
 
+#ifdef CONFIG_PM_SLEEP
 static int cdn_dp_resume(struct device *dev)
 {
 	struct cdn_dp_device *dp = dev_get_drvdata(dev);
@@ -1134,6 +1135,7 @@ static int cdn_dp_resume(struct device *dev)
 
 	return 0;
 }
+#endif
 
 static int cdn_dp_probe(struct platform_device *pdev)
 {
