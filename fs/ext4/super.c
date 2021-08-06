3525eacade5997dd4fff5f613f503db4dd995bb9
Jan Kara <jack@suse.cz>
2021-08-06 11:33:56 +0800

    ext4: fix timer use-after-free on failed mount
    mainline inclusion
    from mainline-v5.12-rc4
    commit 2a4ae3bcdf05b8639406eaa09a2939f3c6dd8e75
    category: bugfix
    issue: #I3ZXZF
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=2a4ae3bcdf05b8639406eaa09a2939f3c6dd8e75
    
    -----------------------------------------------
    
    When filesystem mount fails because of corrupted filesystem we first
    cancel the s_err_report timer reminding fs errors every day and only
    then we flush s_error_work. However s_error_work may report another fs
    error and re-arm timer thus resulting in timer use-after-free. Fix the
    problem by first flushing the work and only after that canceling the
    s_err_report timer.
    
    Reported-by: syzbot+628472a2aac693ab0fcd@syzkaller.appspotmail.com
    Fixes: 2d01ddc86606 ("ext4: save error info to sb through journal if available")
    CC: stable@vger.kernel.org
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://lore.kernel.org/r/20210315165906.2175-1-jack@suse.cz
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Ye Bin <yebin10@huawei.com>
    Reviewed-by: zhangyi (F) <yi.zhang@huawei.com>
    Signed-off-by: Chen Jun <chenjun102@huawei.com>
    Signed-off-by: Yu Changchun <yuchangchun1@huawei.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -5156,8 +5156,8 @@ failed_mount_wq:
 failed_mount3a:
 	ext4_es_unregister_shrinker(sbi);
 failed_mount3:
-	del_timer_sync(&sbi->s_err_report);
 	flush_work(&sbi->s_error_work);
+	del_timer_sync(&sbi->s_err_report);
 	ext4_stop_mmpd(sbi);
 failed_mount2:
 	rcu_read_lock();
