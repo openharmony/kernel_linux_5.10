91f67caba2c0bfdd85ec9480574d129e89374bc5
ChenJie <chenjie174@huawei.com>
2022-06-06 15:15:51 +0800

    add memmgr user and allow it control memcg
    Signed-off-by: ChenJie <chenjie174@huawei.com>

--- include/linux/uidgid.h
+++ include/linux/uidgid.h
@@ -55,6 +55,10 @@ static inline gid_t __kgid_val(kgid_t gid)
 #define GLOBAL_ROOT_UID KUIDT_INIT(0)
 #define GLOBAL_ROOT_GID KGIDT_INIT(0)
 
+#ifdef CONFIG_HYPERHOLD
+#define GLOBAL_MEMMGR_UID KUIDT_INIT(1111)
+#endif
+
 #define INVALID_UID KUIDT_INIT(-1)
 #define INVALID_GID KGIDT_INIT(-1)
 
