83f52952079218ae36acf1d66347d1ab58d40a43
Bibo Mao <maobibo@loongson.cn>
2022-07-20 15:21:52 +0800

    LoongArch: Remove unused variables
    There are some variables never used or referenced, this patch
    removes these varaibles and make the code cleaner.
    
    Change-Id: Ic98c4d0368510184f5a1ea14c0820ae5fb525938
    Reviewed-by: WANG Xuerui <git@xen0n.name>
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- arch/loongarch/loongson64/env.c
+++ arch/loongarch/loongson64/env.c
@@ -14,13 +14,9 @@
 #include <loongson.h>
 
 u64 efi_system_table;
+unsigned long long smp_group[MAX_PACKAGES];
 struct loongson_system_configuration loongson_sysconf;
 EXPORT_SYMBOL(loongson_sysconf);
-
-u64 loongson_chipcfg[MAX_PACKAGES];
-u64 loongson_chiptemp[MAX_PACKAGES];
-u64 loongson_freqctrl[MAX_PACKAGES];
-unsigned long long smp_group[MAX_PACKAGES];
 
 static void __init register_addrs_set(u64 *registers, const u64 addr, int num)
 {
@@ -50,11 +46,7 @@ void __init init_environ(void)
 	efi_memmap_init_early(&data);
 	memblock_reserve(data.phys_map & PAGE_MASK,
 			 PAGE_ALIGN(data.size + (data.phys_map & ~PAGE_MASK)));
-
 	register_addrs_set(smp_group, TO_UNCACHE(0x1fe01000), 16);
-	register_addrs_set(loongson_chipcfg, TO_UNCACHE(0x1fe00180), 16);
-	register_addrs_set(loongson_chiptemp, TO_UNCACHE(0x1fe0019c), 16);
-	register_addrs_set(loongson_freqctrl, TO_UNCACHE(0x1fe001d0), 16);
 }
 
 static int __init init_cpu_fullname(void)
