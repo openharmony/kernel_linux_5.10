26f5f8cb18fb3ce02e3b7544c3d16f40bd3ab2b9
Yang Li <yang.lee@linux.alibaba.com>
2022-08-06 15:19:33 +0800

    LoongArch: Fix unsigned comparison with less than zero
    The return value from the call to get_timer_irq() is int, which can be
    a negative error code. However, the return value is being assigned to an
    unsigned int variable 'irq', so making 'irq' an int.
    
    Eliminate the following coccicheck warning:
    ./arch/loongarch/kernel/time.c:146:5-8: WARNING: Unsigned expression compared with zero: irq < 0
    
    Change-Id: I1c776b755167555dae9afbf248979d2d348f6fd8
    Reported-by: Abaci Robot <abaci@linux.alibaba.com>
    Signed-off-by: Yang Li <yang.lee@linux.alibaba.com>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- arch/loongarch/kernel/time.c
+++ arch/loongarch/kernel/time.c
@@ -130,7 +130,7 @@ void sync_counter(void)
 
 int constant_clockevent_init(void)
 {
-	unsigned int irq;
+	int irq;
 	unsigned int cpu = smp_processor_id();
 	unsigned long min_delta = 0x600;
 	unsigned long max_delta = (1UL << 48) - 1;
