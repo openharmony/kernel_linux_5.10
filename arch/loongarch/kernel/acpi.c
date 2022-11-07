3f52a01408492df9135b89be719bd1cf46b6500b
Huacai Chen <chenhuacai@loongson.cn>
2022-11-07 14:25:41 +0800

    LoongArch: Combine acpi_boot_table_init() and acpi_boot_init()
    Combine acpi_boot_table_init() and acpi_boot_init() since they are very
    simple, and we don't need to check the return value of acpi_boot_init().
    
    Change-Id: I88c4f2e3bf9c1c44daa01e9cd6a85c7ec6097e11
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- arch/loongarch/kernel/acpi.c
+++ arch/loongarch/kernel/acpi.c
@@ -124,23 +124,6 @@ void __iomem *acpi_os_ioremap(acpi_physical_address phys, acpi_size size)
 		return ioremap_cache(phys, size);
 }
 
-void __init acpi_boot_table_init(void)
-{
-	/*
-	 * If acpi_disabled, bail out
-	 */
-	if (acpi_disabled)
-		return;
-
-	/*
-	 * Initialize the ACPI boot-time table parser.
-	 */
-	if (acpi_table_init()) {
-		disable_acpi();
-		return;
-	}
-}
-
 static int set_processor_mask(u32 id, u32 flags)
 {
 
@@ -348,13 +331,21 @@ static void __init acpi_process_madt(void)
 	}
 }
 
-int __init acpi_boot_init(void)
+void __init acpi_boot_table_init(void)
 {
 	/*
 	 * If acpi_disabled, bail out
 	 */
 	if (acpi_disabled)
-		return -1;
+		return;
+
+	/*
+	 * Initialize the ACPI boot-time table parser.
+	 */
+	if (acpi_table_init()) {
+		disable_acpi();
+		return;
+	}
 
 	loongson_sysconf.boot_cpu_id = read_csr_cpuid();
 
@@ -365,8 +356,6 @@ int __init acpi_boot_init(void)
 
 	/* Do not enable ACPI SPCR console by default */
 	acpi_parse_spcr(earlycon_acpi_spcr_enable, false);
-
-	return 0;
 }
 
 #ifdef CONFIG_ACPI_NUMA
