6b7af0c560fe8e8e92b02e7c0d8cb3c225bf9a5b
maojingjing <maojingjing1@huawei.com>
2023-02-13 20:15:32 +0800

    add sharefs file system
    Signed-off-by: maojingjing <maojingjing1@huawei.com>

---
+++ fs/sharefs/inode.c
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * fs/sharefs/inode.c
+ * 
+ * Copyright (c) 1998-2022 Erez Zadok
+ * Copyright (c) 2009	   Shrikar Archak
+ * Copyright (c) 2003-2022 Stony Brook University
+ * Copyright (c) 2003-2022 The Research Foundation of SUNY
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ */
+
+#include "sharefs.h"
+
+static const char *sharefs_get_link(struct dentry *dentry, struct inode *inode,
+				   struct delayed_call *done)
+{
+	DEFINE_DELAYED_CALL(lower_done);
+	struct dentry *lower_dentry;
+	struct path lower_path;
+	char *buf;
+	const char *lower_link;
+
+	if (!dentry)
+		return ERR_PTR(-ECHILD);
+
+	sharefs_get_lower_path(dentry, &lower_path);
+	lower_dentry = lower_path.dentry;
+
+	/*
+	 * get link from lower file system, but use a separate
+	 * delayed_call callback.
+	 */
+	lower_link = vfs_get_link(lower_dentry, &lower_done);
+	if (IS_ERR(lower_link)) {
+		buf = ERR_CAST(lower_link);
+		goto out;
+	}
+
+	/*
+	 * we can't pass lower link up: have to make private copy and
+	 * pass that.
+	 */
+	buf = kstrdup(lower_link, GFP_KERNEL);
+	do_delayed_call(&lower_done);
+	if (!buf) {
+		buf = ERR_PTR(-ENOMEM);
+		goto out;
+	}
+
+	fsstack_copy_attr_atime(d_inode(dentry), d_inode(lower_dentry));
+
+	set_delayed_call(done, kfree_link, buf);
+out:
+	sharefs_put_lower_path(dentry, &lower_path);
+	return buf;
+}
+
+static int sharefs_getattr(const struct path *path, struct kstat *stat, 
+                          u32 request_mask, unsigned int flags)
+{
+	struct path lower_path;
+	int ret;
+
+	sharefs_get_lower_path(path->dentry, &lower_path);
+	ret = vfs_getattr(&lower_path, stat, request_mask, flags);
+	stat->ino = d_inode(path->dentry)->i_ino;
+	stat->uid = d_inode(path->dentry)->i_uid;
+	stat->gid = d_inode(path->dentry)->i_gid;
+	stat->mode = d_inode(path->dentry)->i_mode;
+	sharefs_put_lower_path(path->dentry, &lower_path);
+
+	return ret;
+}
+
+static ssize_t
+sharefs_listxattr(struct dentry *dentry, char *buffer, size_t buffer_size)
+{
+	int err;
+	struct dentry *lower_dentry;
+	struct path lower_path;
+
+	sharefs_get_lower_path(dentry, &lower_path);
+	lower_dentry = lower_path.dentry;
+	if (!(d_inode(lower_dentry)->i_opflags & IOP_XATTR)) {
+		err = -EOPNOTSUPP;
+		goto out;
+	}
+	err = vfs_listxattr(lower_dentry, buffer, buffer_size);
+	if (err)
+		goto out;
+	fsstack_copy_attr_atime(d_inode(dentry),
+				d_inode(lower_path.dentry));
+out:
+	sharefs_put_lower_path(dentry, &lower_path);
+	return err;
+}
+
+static int sharefs_permission(struct inode *inode, int mask)
+{
+	unsigned short mode = inode->i_mode;
+	kuid_t cur_uid = current_fsuid();
+	if (uid_eq(cur_uid, ROOT_UID))
+		return 0;
+	if (uid_eq(cur_uid, inode->i_uid)) {
+		mode >>= 6;
+	} else if (in_group_p(inode->i_gid)) {
+		mode >>= 3;
+	}
+
+	if ((mask & ~mode & (MAY_READ | MAY_WRITE | MAY_EXEC)) == 0)
+		return 0;
+
+	return -EACCES;
+}
+
+const struct inode_operations sharefs_symlink_iops = {
+	.permission	= sharefs_permission,
+	.getattr	= sharefs_getattr,
+	.get_link	= sharefs_get_link,
+	.listxattr	= sharefs_listxattr,
+};
+
+const struct inode_operations sharefs_dir_iops = {
+	.lookup		= sharefs_lookup,
+	.permission	= sharefs_permission,
+	.getattr	= sharefs_getattr,
+	.listxattr	= sharefs_listxattr,
+};
+
+const struct inode_operations sharefs_main_iops = {
+	.permission	= sharefs_permission,
+	.getattr	= sharefs_getattr,
+	.listxattr	= sharefs_listxattr,
+};
---
+++ fs/sharefs/main.c
@@ -0,0 +1,193 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * fs/sharefs/main.c
+ *
+ * Copyright (c) 1998-2022 Erez Zadok
+ * Copyright (c) 2009	   Shrikar Archak
+ * Copyright (c) 2003-2022 Stony Brook University
+ * Copyright (c) 2003-2022 The Research Foundation of SUNY
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ */
+
+#include <linux/module.h>
+#include "sharefs.h"
+#include "authentication.h"
+
+
+struct sharefs_mount_priv {
+       const char *dev_name;
+       const char *raw_data;
+};
+
+/*
+ * There is no need to lock the sharefs_super_info's rwsem as there is no
+ * way anyone can have a reference to the superblock at this point in time.
+ */
+static int sharefs_fill_super(struct super_block *sb, void *data, int silent)
+{
+
+	struct sharefs_mount_priv *priv = (struct sharefs_mount_priv *)data;
+        const char *dev_name = priv->dev_name;
+        const char *raw_data = priv->raw_data;
+
+	int err = 0;
+	struct super_block *lower_sb;
+	struct path lower_path;
+	struct inode *inode;
+
+	/* parse lower path */
+	err = kern_path(dev_name, LOOKUP_FOLLOW | LOOKUP_DIRECTORY,
+			&lower_path);
+	if (err) {
+		printk(KERN_ERR	"sharefs: error accessing "
+		       "lower directory '%s'\n", dev_name);
+		goto out;
+	}
+
+	/* allocate superblock private data */
+	sb->s_fs_info = kzalloc(sizeof(struct sharefs_sb_info), GFP_KERNEL);
+	if (!SHAREFS_SB(sb)) {
+		printk(KERN_CRIT "sharefs: fill_super: out of memory\n");
+		err = -ENOMEM;
+		goto out_pput;
+	}
+
+	/* set the lower superblock field of upper superblock */
+	lower_sb = lower_path.dentry->d_sb;
+	atomic_inc(&lower_sb->s_active);
+	sharefs_set_lower_super(sb, lower_sb);
+
+	/* inherit maxbytes from lower file system */
+	sb->s_maxbytes = lower_sb->s_maxbytes;
+
+	/*
+	 * Our c/m/atime granularity is 1 ns because we may stack on file
+	 * systems whose granularity is as good.
+	 */
+	sb->s_time_gran = 1;
+
+	sb->s_op = &sharefs_sops;
+
+	/* get a new inode and allocate our root dentry */
+	inode = sharefs_iget(sb, d_inode(lower_path.dentry));
+	if (IS_ERR(inode)) {
+		err = PTR_ERR(inode);
+		goto out_pput;
+	}
+	sharefs_root_inode_perm_init(inode);
+	sb->s_root = d_make_root(inode);
+	if (!sb->s_root) {
+		err = -ENOMEM;
+		goto out_pput;
+	}
+	d_set_d_op(sb->s_root, &sharefs_dops);
+
+	err = sharefs_parse_options(sb->s_fs_info, raw_data);
+	if (err)
+		goto out_pput;
+
+	/* link the upper and lower dentries */
+	sb->s_root->d_fsdata = NULL;
+	err = new_dentry_private_data(sb->s_root);
+	if (err)
+		goto out_pput;
+
+	/* if get here: cannot have error */
+
+	/* set the lower dentries for s_root */
+	sharefs_set_lower_path(sb->s_root, &lower_path);
+
+	/*
+	 * No need to call interpose because we already have a positive
+	 * dentry, which was instantiated by d_make_root.  Just need to
+	 * d_rehash it.
+	 */
+	d_rehash(sb->s_root);
+	if (!silent)
+		printk(KERN_INFO
+		       "sharefs: mounted on top of %s type %s\n",
+		       dev_name, lower_sb->s_type->name);
+	goto out; /* all is well */
+
+	/*
+	 * path_put is the only resource we need to free if an error occurred
+	 * because returning an error from this function will cause
+	 * generic_shutdown_super to be called, which will call
+	 * sharefs_put_super, and that function will release any other
+	 * resources we took.
+	 */
+out_pput:
+	path_put(&lower_path);
+out:
+	return err;
+}
+
+struct dentry *sharefs_mount(struct file_system_type *fs_type, int flags,
+			    const char *dev_name, void *raw_data)
+{
+	struct sharefs_mount_priv priv = {
+		.dev_name = dev_name,
+		.raw_data = raw_data,
+	};
+
+	/* sharefs needs a valid dev_name to get the lower_sb's metadata */
+	if (!dev_name || !*dev_name)
+		return ERR_PTR(-EINVAL);
+
+	return mount_nodev(fs_type, flags, &priv,
+			   sharefs_fill_super);
+}
+
+static struct file_system_type sharefs_fs_type = {
+	.owner		= THIS_MODULE,
+	.name		= SHAREFS_NAME,
+	.mount		= sharefs_mount,
+	.kill_sb	= generic_shutdown_super,
+	.fs_flags	= 0,
+};
+MODULE_ALIAS_FS(SHAREFS_NAME);
+
+static int __init init_sharefs_fs(void)
+{
+	int err;
+
+	pr_info("Registering sharefs");
+
+	err = sharefs_init_inode_cache();
+	if (err)
+		goto out_err;
+	err = sharefs_init_dentry_cache();
+	if (err)
+		goto out_err;
+	err = register_filesystem(&sharefs_fs_type);
+	if (err) {
+		sharefs_err("share register failed!");
+		goto out_err;
+	}
+
+	err = sharefs_init_configfs();
+	if (err)
+		goto out_err;
+	return 0;
+out_err:
+	sharefs_exit_configfs();
+	sharefs_destroy_inode_cache();
+	sharefs_destroy_dentry_cache();
+	sharefs_err("sharefs init failed!");
+	return err;
+}
+
+static void __exit exit_sharefs_fs(void)
+{
+	sharefs_destroy_inode_cache();
+	sharefs_destroy_dentry_cache();
+	unregister_filesystem(&sharefs_fs_type);
+	pr_info("Completed sharefs module unload\n");
+}
+
+MODULE_AUTHOR("Jingjing Mao");
+MODULE_DESCRIPTION("Sharefs");
+MODULE_LICENSE("GPL");
+
+module_init(init_sharefs_fs);
+module_exit(exit_sharefs_fs);
