345dfda8987bedb40be2235b59b6262cccee88bb
Ma Ke <make24@iscas.ac.cn>
2025-02-25 10:14:40 +0800

    PCI: Fix reference leak in pci_register_host_bridge()
    stable inclusion
    from stable-v5.10.237
    commit f4db1b2c9ae3d013733c302ee70cac943b7070c0
    category: bugfix
    issue: #ICG2DK
    CVE: CVE-2025-37836
    Signed-off-by: Tengda Wu <wutengda2@huawei.com>
    ---------------------------------------
    
    [ Upstream commit 804443c1f27883926de94c849d91f5b7d7d696e9 ]
    
    If device_register() fails, call put_device() to give up the reference to
    avoid a memory leak, per the comment at device_register().
    
    Found by code review.
    
    Link: https://lore.kernel.org/r/20250225021440.3130264-1-make24@iscas.ac.cn
    Fixes: 37d6a0a6f470 ("PCI: Add pci_register_host_bridge() interface")
    Signed-off-by: Ma Ke <make24@iscas.ac.cn>
    [bhelgaas: squash Dan Carpenter's double free fix from
    https://lore.kernel.org/r/db806a6c-a91b-4e5a-a84b-6b7e01bdac85@stanley.mountain]
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -881,6 +881,7 @@ static int pci_register_host_bridge(struct pci_host_bridge *bridge)
 	resource_size_t offset;
 	LIST_HEAD(resources);
 	struct resource *res;
+	bool bus_registered = false;
 	char addr[64], *fmt;
 	const char *name;
 	int err;
@@ -937,6 +938,7 @@ static int pci_register_host_bridge(struct pci_host_bridge *bridge)
 	name = dev_name(&bus->dev);
 
 	err = device_register(&bus->dev);
+	bus_registered = true;
 	if (err)
 		goto unregister;
 
@@ -996,7 +998,11 @@ unregister:
 	device_del(&bridge->dev);
 
 free:
-	kfree(bus);
+	if (bus_registered)
+		put_device(&bus->dev);
+	else
+		kfree(bus);
+
 	return err;
 }
 
