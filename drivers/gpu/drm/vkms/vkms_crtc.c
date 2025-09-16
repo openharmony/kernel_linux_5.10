dd5cba7586c54ea3b0473fad3d5f042a66046dc8
Thomas Zimmermann <tzimmermann@suse.de>
2025-09-16 10:36:21 +0200

    drm/vkms: Convert to DRM's vblank timer mainline inclusion from mainline-5.10.119 commit 02e2681ffe1addde1fc8c35d05657b16bfa79613 category: bugfix issue: #8662 CVE: CVE-2025-71315 Signed-off-by: wanghao794 <wanghao865@huawei.com> Co-Authored-By: Agent -------------------------------
        drm/vkms: Convert to DRM's vblank timer
    
        Replace vkms' vblank timer with the DRM implementation. The DRM
        code is identical in concept, but differs in implementation.
    
        Vblank timers are covered in vblank helpers and initializer macros,
        so remove the corresponding hrtimer in struct vkms_output. The
        vblank timer calls vkms' custom timeout code via handle_vblank_timeout
        in struct drm_crtc_helper_funcs.
    
        Original-signoff: Thomas Zimmermann <tzimmermann@suse.de>
        Tested-by: Louis Chauvet <louis.chauvet@bootlin.com>
        Reviewed-by: Louis Chauvet <louis.chauvet@bootlin.com>
        Reviewed-by: Javier Martinez Canillas <javierm@redhat.com>
        Link: https://lore.kernel.org/r/20250916083816.30275-4-tzimmermann@suse.de
    
    Signed-off-by: wanghao794 <wanghao865@huawei.com>

--- drivers/gpu/drm/vkms/vkms_crtc.c
+++ drivers/gpu/drm/vkms/vkms_crtc.c
@@ -4,22 +4,15 @@
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_probe_helper.h>
 #include <drm/drm_vblank.h>
+#include <drm/drm_vblank_helper.h>
 
 #include "vkms_drv.h"
 
-static enum hrtimer_restart vkms_vblank_simulate(struct hrtimer *timer)
+static bool vkms_crtc_handle_vblank_timeout(struct drm_crtc *crtc)
 {
-	struct vkms_output *output = container_of(timer, struct vkms_output,
-						  vblank_hrtimer);
-	struct drm_crtc *crtc = &output->crtc;
+	struct vkms_output *output = drm_crtc_to_vkms_output(crtc);
 	struct vkms_crtc_state *state;
-	u64 ret_overrun;
 	bool ret;
-
-	ret_overrun = hrtimer_forward_now(&output->vblank_hrtimer,
-					  output->period_ns);
-	if (ret_overrun != 1)
-		pr_warn("%s: vblank timer overrun\n", __func__);
 
 	spin_lock(&output->lock);
 	ret = drm_crtc_handle_vblank(crtc);
@@ -50,22 +43,13 @@ static enum hrtimer_restart vkms_vblank_simulate(struct hrtimer *timer)
 			DRM_DEBUG_DRIVER("Composer worker already queued\n");
 	}
 
-	return HRTIMER_RESTART;
-}
-
-static int vkms_enable_vblank(struct drm_crtc *crtc)
-{
 	struct drm_device *dev = crtc->dev;
 	unsigned int pipe = drm_crtc_index(crtc);
-	struct drm_vblank_crtc *vblank = &dev->vblank[pipe];
-	struct vkms_output *out = drm_crtc_to_vkms_output(crtc);
-
 	drm_calc_timestamping_constants(crtc, &crtc->mode);
 
 	hrtimer_init(&out->vblank_hrtimer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	out->vblank_hrtimer.function = &vkms_vblank_simulate;
 	out->period_ns = ktime_set(0, vblank->framedur_ns);
-	hrtimer_start(&out->vblank_hrtimer, out->period_ns, HRTIMER_MODE_REL);
 
 	return 0;
 }
@@ -79,7 +63,6 @@ static void vkms_disable_vblank(struct drm_crtc *crtc)
 
 static bool vkms_get_vblank_timestamp(struct drm_crtc *crtc,
 				      int *max_error, ktime_t *vblank_time,
-				      bool in_vblank_irq)
 {
 	struct drm_device *dev = crtc->dev;
 	unsigned int pipe = crtc->index;
@@ -94,13 +77,6 @@ static bool vkms_get_vblank_timestamp(struct drm_crtc *crtc,
 
 	*vblank_time = READ_ONCE(output->vblank_hrtimer.node.expires);
 
-	if (WARN_ON(*vblank_time == vblank->time))
-		return true;
-
-	/*
-	 * To prevent races we roll the hrtimer forward before we do any
-	 * interrupt processing - this is how real hw works (the interrupt is
-	 * only generated after all the vblank registers are updated) and what
 	 * the vblank core expects. Therefore we need to always correct the
 	 * timestampe by one frame.
 	 */
@@ -160,9 +136,7 @@ static const struct drm_crtc_funcs vkms_crtc_funcs = {
 	.reset                  = vkms_atomic_crtc_reset,
 	.atomic_duplicate_state = vkms_atomic_crtc_duplicate_state,
 	.atomic_destroy_state   = vkms_atomic_crtc_destroy_state,
-	.enable_vblank		= vkms_enable_vblank,
-	.disable_vblank		= vkms_disable_vblank,
-	.get_vblank_timestamp	= vkms_get_vblank_timestamp,
+	DRM_CRTC_VBLANK_TIMER_FUNCS,
 	.get_crc_sources	= vkms_get_crc_sources,
 	.set_crc_source		= vkms_set_crc_source,
 	.verify_crc_source	= vkms_verify_crc_source,
@@ -264,8 +238,9 @@ static const struct drm_crtc_helper_funcs vkms_crtc_helper_funcs = {
 	.atomic_check	= vkms_crtc_atomic_check,
 	.atomic_begin	= vkms_crtc_atomic_begin,
 	.atomic_flush	= vkms_crtc_atomic_flush,
-	.atomic_enable	= vkms_crtc_atomic_enable,
-	.atomic_disable	= vkms_crtc_atomic_disable,
+	.atomic_enable	= drm_crtc_vblank_atomic_enable,
+	.atomic_disable	= drm_crtc_vblank_atomic_disable,
+	.handle_vblank_timeout = vkms_crtc_handle_vblank_timeout,
 };
 
 int vkms_crtc_init(struct drm_device *dev, struct drm_crtc *crtc,
