cbb4ac8642b1366095835b952c85cca15ec927c4
Eric Dumazet <edumazet@google.com>
2026-03-02 20:55:27 +0000

    tcp: secure_seq: add back ports to TS offset mainline inclusion from mainline-5.10.100 commit 165573e41f2f66ef98940cf65f838b2cb575d9d1 category: bugfix issue: #8294 CVE: CVE-2026-23247 Signed-off-by: 姜小林 <jiangxiaolin11@huawei.com> Co-Authored-By: Agent -------------------------------
        tcp: secure_seq: add back ports to TS offset
    
        This reverts 28ee1b746f49 ("secure_seq: downgrade to per-host timestamp offsets")
    
        tcp_tw_recycle went away in 2017.
    
        Zhouyan Deng reported off-path TCP source port leakage via
        SYN cookie side-channel that can be fixed in multiple ways.
    
        One of them is to bring back TCP ports in TS offset randomization.
    
        As a bonus, we perform a single siphash() computation
        to provide both an ISN and a TS offset.
    
        Fixes: 28ee1b746f49 ("secure_seq: downgrade to per-host timestamp offsets")
        Reported-by: Zhouyan Deng <dengzhouyan_nwpu@163.com>
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Reviewed-by: Kuniyuki Iwashima <kuniyu@google.com>
        Acked-by: Florian Westphal <fw@strlen.de>
        Link: https://patch.msgid.link/20260302205527.1982836-1-edumazet@google.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Change-Id: Idbcac3430c5ba7da209515afaafa8fcb3616efbf
    Signed-off-by: 姜小林 <jiangxiaolin11@huawei.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -18,6 +18,7 @@
 
 #include <linux/list.h>
 #include <linux/tcp.h>
+#include <net/secure_seq.h>
 #include <linux/bug.h>
 #include <linux/slab.h>
 #include <linux/cache.h>
@@ -2065,8 +2066,9 @@ struct tcp_request_sock_ops {
 #endif
 	struct dst_entry *(*route_req)(const struct sock *sk, struct flowi *fl,
 				       const struct request_sock *req);
-	u32 (*init_seq)(const struct sk_buff *skb);
-	u32 (*init_ts_off)(const struct net *net, const struct sk_buff *skb);
+	union tcp_seq_and_ts_off (*init_seq_and_ts_off)(
+					const struct net *net,
+					const struct sk_buff *skb);
 	int (*send_synack)(const struct sock *sk, struct dst_entry *dst,
 			   struct flowi *fl, struct request_sock *req,
 			   struct tcp_fastopen_cookie *foc,
